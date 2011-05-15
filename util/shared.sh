bb88729a8caae72faee90638956e6fcdbd896b97
Janmejay Singh <singh.janmejay@gmail.com>
2011-05-15 09:12:53 +0530

    it seems layout is not processed like other pages and jekyll declarations are not processed, so using static content as far as possible

--- util/shared.sh
+++ util/shared.sh
@@ -4,7 +4,7 @@ GOOGLE_CDN_END='<!--GOOGLE-CDN-END-->'
 LOCAL_CDN_END='<!--LOCAL-DEV-END-->'
 
 function cdn_transform {
-    for file_path in `ls _includes/layout_*`; do 
+    for file_path in `ls _layouts/*.html 404.html`; do 
         file_base_name=${file_path##*/}
         cat $file_path | sed -r "s#$1#$2#g" > tmp/$file_base_name
         cat tmp/$file_base_name | sed -r "s#$3#$4#g" > $file_path
